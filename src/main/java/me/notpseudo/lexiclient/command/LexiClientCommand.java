a959459511f56065f5ed66d05c0c04f9f6cad5ca
NotPseudo <hongxwen@gmail.com>
2024-06-15 17:21:31 -0400

    OneConfig generated main command to open mod configuration menu

---
+++ src/main/java/me/notpseudo/lexiclient/command/LexiClientCommand.java
@@ -0,0 +1,21 @@
+package me.notpseudo.lexiclient.command;
+
+import me.notpseudo.lexiclient.LexiClient;
+import cc.polyfrost.oneconfig.utils.commands.annotations.Command;
+import cc.polyfrost.oneconfig.utils.commands.annotations.Main;
+
+/**
+ * An example command implementing the Command api of OneConfig.
+ * Registered in LexiClient.java with `CommandManager.INSTANCE.registerCommand(new LexiClientCommand());`
+ *
+ * @see Command
+ * @see Main
+ * @see LexiClient
+ */
+@Command(value = LexiClient.MODID, description = "Access the " + LexiClient.NAME + " GUI.")
+public class LexiClientCommand {
+    @Main
+    private void handle() {
+        LexiClient.INSTANCE.config.openGui();
+    }
+}
