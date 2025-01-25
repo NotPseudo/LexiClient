3b979e41298a3f6b5d6886fa42a3992edaf634ef
NotPseudo <hongxwen@gmail.com>
2025-01-24 19:22:11 -0500

    Added new config options for dungeon enter messages

--- src/main/java/me/notpseudo/lexiclient/config/LexiConfig.java
+++ src/main/java/me/notpseudo/lexiclient/config/LexiConfig.java
@@ -1,14 +1,17 @@
 package me.notpseudo.lexiclient.config;
 
-import cc.polyfrost.oneconfig.config.annotations.HUD;
-import cc.polyfrost.oneconfig.config.annotations.Slider;
-import cc.polyfrost.oneconfig.config.annotations.Switch;
+import cc.polyfrost.oneconfig.config.annotations.*;
+import cc.polyfrost.oneconfig.config.annotations.Number;
 import me.notpseudo.lexiclient.LexiClient;
 import cc.polyfrost.oneconfig.config.Config;
 
 import cc.polyfrost.oneconfig.config.data.Mod;
 import cc.polyfrost.oneconfig.config.data.ModType;
 import me.notpseudo.lexiclient.hud.DominusHud;
+import me.notpseudo.lexiclient.utils.ChatUtils;
+import net.minecraft.client.audio.SoundCategory;
+
+import static me.notpseudo.lexiclient.LexiClient.mc;
 
 
 /**
@@ -16,6 +19,357 @@ import me.notpseudo.lexiclient.hud.DominusHud;
  * See <a href="https://docs.polyfrost.cc/oneconfig/config/adding-options">this link</a> for more config Options
  */
 public class LexiConfig extends Config {
+
+    @Switch(
+            name = "Show Position Titles",
+            description = "Displays titles when receiving positional messages",
+            category = "Dungeons",
+            subcategory = "Position Titles"
+    )
+    public static boolean showPosTitle = false;
+
+    @Switch(
+            name = "Play Sound for Positional",
+            description = "Will play a sound when receiving positional messages",
+            category = "Dungeons",
+            subcategory = "Position Titles"
+    )
+    public static boolean recPosSound = false;
+
+    @Slider(
+            name = "Positional Sound Volume",
+            description = "Volume of receive positional message sound",
+            category = "Dungeons",
+            subcategory = "Position Titles",
+            min = 0f,
+            max = 1f,
+            step = 0
+    )
+    public static float recPosSoundVolume = .7f;
+
+    @Button(
+            name = "Positional Volume Test",
+            category = "Dungeons",
+            subcategory = "Position Titles",
+            text = "Test Sound",
+            size = 2
+    )
+    void demoPosTitleVolume() {
+        float prevMaster = mc.gameSettings.getSoundLevel(SoundCategory.MASTER);
+        mc.gameSettings.setSoundLevel(SoundCategory.MASTER, 1f);
+        float prevNote = mc.gameSettings.getSoundLevel(SoundCategory.RECORDS);
+        mc.gameSettings.setSoundLevel(SoundCategory.RECORDS, 1f);
+        mc.thePlayer.playSound(
+                "note.pling",
+                recPosSoundVolume,
+                1f
+        );
+        mc.gameSettings.setSoundLevel(SoundCategory.MASTER, prevMaster);
+        mc.gameSettings.setSoundLevel(SoundCategory.RECORDS, prevNote);
+    }
+
+    @Dropdown(
+            name = "Position Title Color",
+            options = {"Black", "Dark Blue", "Dark Green", "Dark Aqua", "Dark Red", "Dark Purple", "Gold", "Gray", "Dark Gray", "Blue", "Green", "Aqua", "Red", "Light Purple", "Yellow", "White"},
+            description = "The color of the title that displays",
+            category = "Dungeons",
+            subcategory = "Position Titles",
+            size = 2
+    )
+    public static int positionTitleColor = 11;
+
+    @Slider(
+            name = "Scale",
+            description = "Size of the title",
+            category = "Dungeons",
+            subcategory = "Position Titles",
+            min = 0f,
+            max = 1f,
+            step = 0
+    )
+    public static float positionTitleSize = .7f;
+
+    @Switch(
+            name = "Use Custom Screen Position",
+            description = "If enabled, will use custom location on your screen for the title. If disabled, will just try to place the message at the center of your screen",
+            category = "Dungeons",
+            subcategory = "Position Titles",
+            size = 2
+    )
+    public static boolean posTitleUseCustomScreen = false;
+
+    @Number(
+            name = "SS X", min = 0, max = 7680, category = "Dungeons", subcategory = "Position Titles")
+    public static int posTitleX = 960;
+
+    @Number(
+            name = "SS Y", min = 0, max = 4320, category = "Dungeons", subcategory = "Position Titles")
+    public static int posTitleY = 540;
+
+    @Text(
+            name = "SS Message",
+            placeholder = "At SS!",
+            description = "The message to look for from your teammates when they are at Simon Says",
+            category = "Dungeons", subcategory = "Position Titles"
+    )
+    public static String ssTitleMessage = "At SS!";
+
+    @Text(
+            name = "EE2 Message",
+            placeholder = "At Early Enter 2!",
+            description = "The message to look for from your teammates when they are at Early Enter 2",
+            category = "Dungeons", subcategory = "Position Titles"
+    )
+    public static String ee2TitleMessage = "At Early Enter 2!";
+
+    @Text(
+            name = "EE3 Message",
+            placeholder = "At Early Enter 3!",
+            description = "The message to look for from your teammates when they are at Early Enter 3",
+            category = "Dungeons", subcategory = "Position Titles"
+    )
+    public static String ee3TitleMessage = "At Early Enter 3!";
+
+    @Text(
+            name = "EE4 Message",
+            placeholder = "At Early Enter 4!",
+            description = "The message to look for from your teammates when they are at Early Enter 4",
+            category = "Dungeons", subcategory = "Position Titles"
+    )
+    public static String ee4TitleMessage = "At Early Enter 4!";
+
+    @Text(
+            name = "Core Message",
+            placeholder = "At Core!",
+            description = "The message to look for from your teammates when they are at core",
+            category = "Dungeons", subcategory = "Position Titles"
+    )
+    public static String coreTitleMessage = "At Core!";
+
+    @Button(
+            name = "Restore Position Title Defaults",    // name beside the button
+            text = "Reset",
+            description = "Reset default messages for Position Titles",
+            category = "Dungeons", subcategory = "Position Titles"
+    )
+    static void resetPosTitles() {
+        ssTitleMessage = "At SS!";
+        ee2TitleMessage = "At Early Enter 2!";
+        ee3TitleMessage = "At Early Enter 3!";
+        ee4TitleMessage = "At Early Enter 4!";
+        coreTitleMessage = "At Core!";
+    };
+
+    @Switch(
+            name = "Send Positional Messages",
+            description = "Automatically send messages when your player goes within coordinates",
+            category = "Dungeons"
+    )
+    public static boolean sendPosMessages = false;
+
+    @Number(
+            name = "SS X", min = -1000, max = 1000, category = "Dungeons", subcategory = "Simon Says")
+    public static int ssx = 107;
+
+    @Number(
+            name = "SS Y", min = -1000, max = 1000, category = "Dungeons", subcategory = "Simon Says")
+    public static int ssy = 120;
+
+    @Number(
+            name = "SS Z", min = -1000, max = 1000, category = "Dungeons", subcategory = "Simon Says")
+    public static int ssz = 93;
+
+    @Number(
+            name = "SS X2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Simon Says")
+    public static int ssx2 = 110;
+
+    @Number(
+            name = "SS Y2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Simon Says")
+    public static int ssy2 = 120;
+
+    @Number(
+            name = "SS Z2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Simon Says")
+    public static int ssz2 = 94;
+
+    @Text(
+            name = "SS Message",
+            placeholder = "At SS!",
+            description = "The message to send when you are at your Simon Says coordinates",
+            category = "Dungeons", subcategory = "Simon Says"
+    )
+    public static String ssMessage = "At SS!";
+
+    @Number(
+            name = "EE2 X", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 2")
+    public static int ee2x = 55;
+
+    @Number(
+            name = "EE2 Y", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 2")
+    public static int ee2y = 107;
+
+    @Number(
+            name = "EE2 Z", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 2")
+    public static int ee2z = 129;
+
+    @Number(
+            name = "EE2 X2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 2")
+    public static int ee2x2 = 60;
+
+    @Number(
+            name = "EE2 Y2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 2")
+    public static int ee2y2 = 110;
+
+    @Number(
+            name = "EE2 Z2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 2")
+    public static int ee2z2 = 133;
+
+    @Text(
+            name = "EE2 Message",
+            placeholder = "At Early Enter 2!",
+            description = "The message to send when you are at your Early Enter 2 coordinates",
+            category = "Dungeons", subcategory = "Early Enter 2"
+    )
+    public static String ee2Message = "At Early Enter 2!";
+
+    @Number(
+            name = "EE3 X", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 3")
+    public static int ee3x = 5;
+
+    @Number(
+            name = "EE3 Y", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 3")
+    public static int ee3y = 110;
+
+    @Number(
+            name = "EE3 Z", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 3")
+    public static int ee3z = 103;
+
+    @Number(
+            name = "EE3 X2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 3")
+    public static int ee3x2 = 7;
+
+    @Number(
+            name = "EE3 Y2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 3")
+    public static int ee3y2 = 115;
+
+    @Number(
+            name = "EE3 Z2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 3")
+    public static int ee3z2 = 107;
+
+    @Text(
+            name = "EE3 Message",
+            placeholder = "At Early Enter 3!",
+            description = "The message to send when you are at your Early Enter 3 coordinates",
+            category = "Dungeons", subcategory = "Early Enter 3"
+    )
+    public static String ee3Message = "At Early Enter 3!";
+
+    @Number(
+            name = "EE4 X", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 4")
+    public static int ee4x = 50;
+
+    @Number(
+            name = "EE4 Y", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 4")
+    public static int ee4y = 113;
+
+    @Number(
+            name = "EE4 Z", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 4")
+    public static int ee4z = 50;
+
+    @Number(
+            name = "EE4 X2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 4")
+    public static int ee4x2 = 58;
+
+    @Number(
+            name = "EE4 Y2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 4")
+    public static int ee4y2 = 117;
+
+    @Number(
+            name = "EE4 Z2", min = -1000, max = 1000, category = "Dungeons", subcategory = "Early Enter 4")
+    public static int ee4z2 = 54;
+
+    @Text(
+            name = "EE4 Message",
+            placeholder = "At Early Enter 4!",
+            description = "The message to send when you are at your Early Enter 4 coordinates",
+            category = "Dungeons", subcategory = "Early Enter 4"
+    )
+    public static String ee4Message = "At Early Enter 4!";
+
+    @Number(
+            name = "Core X", min = -1000, max = 1000, category = "Dungeons", subcategory = "Core")
+    public static int corex = 49;
+
+    @Number(
+            name = "Core Y", min = -1000, max = 1000, category = "Dungeons", subcategory = "Core")
+    public static int corey = 111;
+
+    @Number(
+            name = "Core Z", min = -1000, max = 1000, category = "Dungeons", subcategory = "Core")
+    public static int corez = 55;
+
+    @Number(
+            name = "Core X", min = -1000, max = 1000, category = "Dungeons", subcategory = "Core")
+    public static int corex2 = 60;
+
+    @Number(
+            name = "Core Y", min = -1000, max = 1000, category = "Dungeons", subcategory = "Core")
+    public static int corey2 = 118;
+
+    @Number(
+            name = "Core Z", min = -1000, max = 1000, category = "Dungeons", subcategory = "Core")
+    public static int corez2 = 60;
+
+    @Text(
+            name = "Core Message",
+            placeholder = "At Core!",
+            description = "The message to send when you are at your Core coordinates",
+            category = "Dungeons", subcategory = "Core"
+    )
+    public static String coreMessage = "At Core!";
+
+    @Button(
+            name = "Restore Send Positional Defaults",    // name beside the button
+            text = "Reset",
+            description = "Reset default coordinates and messages for Send Positional Messages", // text on the button itself
+            category = "Dungeons"
+    )
+    static void resetSendPosMessages() {    // using a lambda to create the runnable interface.
+        ssx = 107;
+        ssy = 120;
+        ssz = 93;
+        ssx2 = 110;
+        ssy2 = 120;
+        ssz2 = 94;
+        ssMessage = "At SS!";
+        ee2x = 55;
+        ee2y = 107;
+        ee2z = 129;
+        ee2x2 = 60;
+        ee2y2 = 110;
+        ee2z2 = 133;
+        ee2Message = "At Early Enter 2!";
+        ee3x = 5;
+        ee3y = 110;
+        ee3z = 103;
+        ee3x2 = 7;
+        ee3y2 = 115;
+        ee3z2 = 107;
+        ee3Message = "At Early Enter 3!";
+        ee4x = 50;
+        ee4y = 113;
+        ee4z = 50;
+        ee4x2 = 58;
+        ee4y2 = 117;
+        ee4z2 = 54;
+        ee4Message = "At Early Enter 4!";
+        corex = 49;
+        corey = 111;
+        corez = 55;
+        corex2 = 60;
+        corey2 = 118;
+        corez2 = 60;
+        coreMessage = "At Core!";
+    };
 
     @HUD(
             name = "Dominus Hud",
@@ -27,13 +381,13 @@ public class LexiConfig extends Config {
             name = "Lexi Rat",
             category = "Dumb Shit"
     )
-    public static boolean lexiRat = true;
+    public static boolean lexiRat = false;
 
     @Switch(
             name = "Dumb Messages",
             category = "Dumb Shit"
     )
-    public static boolean dumbMessages = true;
+    public static boolean dumbMessages = false;
 
     @Slider(
             name = "Dumb Message Timer",
@@ -43,10 +397,40 @@ public class LexiConfig extends Config {
     )
     public static float dumbMessageTime = 15f;
 
+    @Switch(
+            name = "Test Mode",
+            description = "Do not enable as this will break many features",
+            category = "Testing"
+    )
+    public static boolean testMode = false;
+
+    @Text(
+            name = "Test Message",
+            description = "The message to send to your client",
+            category = "Testing"
+    )
+    public static String testMessage = "";
+
+    @Button(
+            name = "Test Message", text = "Test", category = "Testing"
+    )
+    Runnable sendTestMessage = () -> {
+        ChatUtils.info(testMessage, false);
+    };
 
     public LexiConfig() {
         super(new Mod(LexiClient.NAME, ModType.SKYBLOCK), LexiClient.MODID + ".json");
         initialize();
+        addDependency("testMessage", "testMode");
+        addDependency("sendTestMessage", "testMode");
+        addDependency("positionTitleColor", "showPosTitle");
+        addDependency("positionTitleSize", "showPosTitle");
+        addDependency("posTitleUseCustomScreen", "showPosTitle");
+        addDependency("posTitleX", "showPosTitle");
+        addDependency("posTitleY", "showPosTitle");
+        addDependency("recPosSound", "showPosTitle");
+        addDependency("recPosSoundVolume", "showPosTitle");
+        addDependency("demoPosTitleVolume", "showPosTitle");
     }
 }
 
