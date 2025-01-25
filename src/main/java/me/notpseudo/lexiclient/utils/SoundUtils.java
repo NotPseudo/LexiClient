e929428aa0a375437892aee93f823ba87714cbb2
NotPseudo <hongxwen@gmail.com>
2025-01-24 19:26:48 -0500

    Added soundutils to play sounds

---
+++ src/main/java/me/notpseudo/lexiclient/utils/SoundUtils.java
@@ -0,0 +1,23 @@
+package me.notpseudo.lexiclient.utils;
+
+import net.minecraft.client.audio.SoundCategory;
+
+import static me.notpseudo.lexiclient.LexiClient.mc;
+
+public class SoundUtils {
+
+    public static void playPling(float volume) {
+        float prevMaster = mc.gameSettings.getSoundLevel(SoundCategory.MASTER);
+        mc.gameSettings.setSoundLevel(SoundCategory.MASTER, 1f);
+        float prevNote = mc.gameSettings.getSoundLevel(SoundCategory.RECORDS);
+        mc.gameSettings.setSoundLevel(SoundCategory.RECORDS, 1f);
+        mc.thePlayer.playSound(
+                "note.pling",
+                volume,
+                1f
+        );
+        mc.gameSettings.setSoundLevel(SoundCategory.MASTER, prevMaster);
+        mc.gameSettings.setSoundLevel(SoundCategory.RECORDS, prevNote);
+    }
+
+}
