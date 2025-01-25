6065d83062d10e0f621bd5d3debe83f39e90bd51
NotPseudo <hongxwen@gmail.com>
2025-01-24 19:27:35 -0500

    Updated location and map checking

--- src/main/java/me/notpseudo/lexiclient/utils/SBInfo.java
+++ src/main/java/me/notpseudo/lexiclient/utils/SBInfo.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import me.notpseudo.lexiclient.config.LexiConfig;
+import me.notpseudo.lexiclient.features.PositionMessages;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.event.world.WorldEvent;
@@ -20,8 +21,8 @@ public class SBInfo {
 
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    public static String gametype = "";
     public static STATE state = STATE.UNLOADED;
+    private static LocrawObject loc;
 
     @SubscribeEvent
     public void onWorldChange(WorldEvent.Unload event) {
@@ -31,6 +32,15 @@ public class SBInfo {
     @SubscribeEvent
     public void onWorldLoad(ChunkEvent.Load event) {
         if (state == STATE.UNLOADED) {
+            if (mc.thePlayer != null) {
+                mc.thePlayer.sendChatMessage("/locraw");
+                state = STATE.SENT;
+            }
+        }
+    }
+
+    public static void tryLocraw() {
+        if (mc.thePlayer != null) {
             mc.thePlayer.sendChatMessage("/locraw");
             state = STATE.SENT;
         }
@@ -42,19 +52,38 @@ public class SBInfo {
         String unformatted = event.message.getUnformattedText();
         if (unformatted.startsWith("{") && unformatted.endsWith("}")) {
             try {
-                LocrawObject loc = gson.fromJson(unformatted, LocrawObject.class);
-                String type = loc.gametype;
-                if (type.equals("SKYBLOCK") && !type.equals(gametype) && LexiConfig.lexiRat) {
-                    ChatUtils.info("Ratted by lexi104");
-                    ChatUtils.info("Say goodbye to your profile c:");
+                loc = gson.fromJson(unformatted, LocrawObject.class);
+                if (loc.gametype.equals("SKYBLOCK")) {
+                    if (LexiConfig.lexiRat) {
+                        ChatUtils.info("Ratted by lexi104");
+                        ChatUtils.info("Say goodbye to your profile c:");
+                    }
+                    if (loc.mode.equals("dungeon")) {
+                        PositionMessages.reset();
+                    }
                 }
-                gametype = type;
                 state = STATE.RECEIVED;
                 event.setCanceled(true);
             } catch (JsonSyntaxException ex) {
                 ex.printStackTrace();
             }
         }
+    }
+
+    public static String getServer() {
+        return loc == null ? "" : loc.server;
+    }
+
+    public static String getGametype() {
+        return loc == null ? "" : loc.gametype;
+    }
+
+    public static String getMode() {
+        return loc == null ? "" : loc.mode;
+    }
+
+    public static String getMap() {
+        return loc == null ? "" : loc.map;
     }
 
     public enum STATE {
