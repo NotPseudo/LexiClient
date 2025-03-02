2071f3ab9a11badd274b470bc24aecdfb0982a39
NotPseudo <hongxwen@gmail.com>
2025-03-02 04:42:54 -0500

    Added customizable countdown timer for when Wither Relics spawn

---
+++ src/main/java/me/notpseudo/lexiclient/features/RelicSpawnTimer.java
@@ -0,0 +1,65 @@
+package me.notpseudo.lexiclient.features;
+
+import me.notpseudo.lexiclient.LexiClient;
+import me.notpseudo.lexiclient.config.LexiConfig;
+import me.notpseudo.lexiclient.utils.SBInfo;
+import me.notpseudo.lexiclient.utils.TextUtils;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RelicSpawnTimer {
+
+    private static final String NECRON_DEFEAT_REGEX = "^\\[BOSS\\] Necron: All this, for nothing\\.\\.\\.$";
+    private static final Pattern DEFEAT_PATTERN = Pattern.compile(NECRON_DEFEAT_REGEX);
+    private static int ticksLeft = LexiConfig.relicSpawnTicks;
+    private static boolean continueUpdate;
+
+    @SubscribeEvent
+    public void onWorldChange(WorldEvent.Load event) {
+        reset();
+    }
+
+    @SubscribeEvent
+    public void onChatReceive(ClientChatReceivedEvent event) {
+        if (!LexiConfig.relicSpawnHud.isEnabled()) return;
+        if (!SBInfo.getMode().equals("dungeon") && !LexiConfig.testMode) return;
+        if (event.type != 0) return;
+        String unformatted = event.message.getUnformattedText();
+        String stripped = TextUtils.stripColor(unformatted);
+        Matcher matcher = DEFEAT_PATTERN.matcher(stripped);
+        if (matcher.matches()) {
+            continueUpdate = true;
+        }
+    }
+
+    @SubscribeEvent
+    public void onTick(TickEvent.ClientTickEvent event) {
+        if (!continueUpdate) return;
+        if (ticksLeft-- <= -40) {
+            reset();
+        }
+    }
+
+    public static void testRelicSpawnTimer() {
+        continueUpdate = true;
+    }
+
+    public static int getTicksLeft() {
+        return ticksLeft;
+    }
+
+    public static boolean stillCounting() {
+        return continueUpdate;
+    }
+
+    private static void reset() {
+        ticksLeft = LexiConfig.relicSpawnTicks;
+        continueUpdate = false;
+    }
+
+}
---
+++ src/main/java/me/notpseudo/lexiclient/hud/RelicSpawnHud.java
@@ -0,0 +1,27 @@
+package me.notpseudo.lexiclient.hud;
+
+import cc.polyfrost.oneconfig.hud.TextHud;
+import me.notpseudo.lexiclient.config.LexiConfig;
+import me.notpseudo.lexiclient.features.RelicSpawnTimer;
+
+import java.util.List;
+
+public class RelicSpawnHud extends TextHud {
+
+    public RelicSpawnHud() {
+        super(false);
+    }
+
+    @Override
+    protected void getLines(List<String> lines, boolean example) {
+        int showTicks = Math.max(RelicSpawnTimer.getTicksLeft(), 0);
+        if (example || RelicSpawnTimer.stillCounting()) {
+            if (LexiConfig.relicSpawnInSecs) {
+                lines.add(String.format("%.2fs", (showTicks * .05)));
+            } else {
+                lines.add(showTicks + "t");
+            }
+        }
+    }
+
+}
