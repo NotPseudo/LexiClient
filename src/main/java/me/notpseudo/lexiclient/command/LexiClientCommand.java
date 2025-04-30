d41ca41fbb08dc37abc2c03a43cc5444418cf927
NotPseudo <hongxwen@gmail.com>
2025-04-30 15:21:24 -0400

    Add more functions to the main command to allow loading configs

--- src/main/java/me/notpseudo/lexiclient/command/LexiClientCommand.java
+++ src/main/java/me/notpseudo/lexiclient/command/LexiClientCommand.java
@@ -1,8 +1,13 @@
 package me.notpseudo.lexiclient.command;
 
+import cc.polyfrost.oneconfig.utils.commands.annotations.SubCommand;
+import cc.polyfrost.oneconfig.utils.commands.annotations.SubCommandGroup;
 import me.notpseudo.lexiclient.LexiClient;
 import cc.polyfrost.oneconfig.utils.commands.annotations.Command;
 import cc.polyfrost.oneconfig.utils.commands.annotations.Main;
+import me.notpseudo.lexiclient.features.GiftNotifications;
+import me.notpseudo.lexiclient.features.PositionMessages;
+import me.notpseudo.lexiclient.utils.ChatUtils;
 
 /**
  * An example command implementing the Command api of OneConfig.
@@ -12,9 +17,38 @@ import cc.polyfrost.oneconfig.utils.commands.annotations.Main;
  * @see Main
  * @see LexiClient
  */
-@Command(value = LexiClient.MODID, aliases = {"lexi", "lc"}, description = "Access the " + LexiClient.NAME + " GUI.")
+@Command(value = LexiClient.MODID, aliases = {"lexi", "lc"}, description = "Access " + LexiClient.NAME)
 public class LexiClientCommand {
-    @Main
+
+    @SubCommand(description = "Prints the help and command descriptions", aliases = {"h"})
+    private void help() {
+        ChatUtils.info("/lexi - Opens the GUI");
+        ChatUtils.info("/lexi help - Displays this help text");
+        ChatUtils.info("/lexi load gift - Loads the gift config file");
+        ChatUtils.info("/lexi load posmessage - Loads the posmessage config file");
+    }
+
+    @SubCommandGroup(value = "load", aliases = "l")
+    private class LoadGroup {
+
+        @SubCommand(description = "Loads the gifts you care about config file from config/lexiclient/goodgifts.txt", aliases = {"gifts", "goodgifts"})
+        private void gift() {
+            if (GiftNotifications.loadGoodGifts()) {
+                ChatUtils.info("Loaded gifts");
+            } else {
+                ChatUtils.info("Failed to load gifts");
+            }
+        }
+
+        @SubCommand(description = "Loads the positional messages config file from config/lexiclient/positional_messages.json", aliases = {"pm", "posmessages", "positionalmessages"})
+        private void posmessage() {
+            PositionMessages.loadPosMessageConfig();
+            ChatUtils.info("Loaded positional messages");
+        }
+
+    }
+
+    @Main(description = "Opens the GUI")
     private void handle() {
         LexiClient.INSTANCE.config.openGui();
     }
