7599a8775941ca47814a703c5353f4e8454a2418
NotPseudo <hongxwen@gmail.com>
2025-04-30 15:16:38 -0400

    Make playSound more universal

--- src/main/java/me/notpseudo/lexiclient/utils/SoundUtils.java
+++ src/main/java/me/notpseudo/lexiclient/utils/SoundUtils.java
@@ -2,22 +2,33 @@ package me.notpseudo.lexiclient.utils;
 
 import net.minecraft.client.audio.SoundCategory;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static me.notpseudo.lexiclient.LexiClient.mc;
 
 public class SoundUtils {
 
+
+    private static final List<SoundCategory> CATEGORIES = Arrays.asList(SoundCategory.values());
+
     public static void playPling(float volume, float pitch) {
-        float prevMaster = mc.gameSettings.getSoundLevel(SoundCategory.MASTER);
-        mc.gameSettings.setSoundLevel(SoundCategory.MASTER, 1f);
-        float prevNote = mc.gameSettings.getSoundLevel(SoundCategory.RECORDS);
-        mc.gameSettings.setSoundLevel(SoundCategory.RECORDS, 1f);
-        mc.thePlayer.playSound(
-                "note.pling",
-                volume,
-                pitch
-        );
-        mc.gameSettings.setSoundLevel(SoundCategory.MASTER, prevMaster);
-        mc.gameSettings.setSoundLevel(SoundCategory.RECORDS, prevNote);
+        playSound("note.pling", volume, pitch);
+    }
+
+    public static void playSound(String sound, float volume, float pitch) {
+        if (mc.thePlayer == null) return;
+        float[] prevVolumes = new float[CATEGORIES.size()];
+        CATEGORIES.forEach(category -> {
+            prevVolumes[category.ordinal()] = mc.gameSettings.getSoundLevel(category);
+        });
+        CATEGORIES.forEach(category -> {
+            mc.gameSettings.setSoundLevel(category, 1f);
+        });
+        mc.thePlayer.playSound(sound, volume, pitch);
+        CATEGORIES.forEach(category -> {
+            mc.gameSettings.setSoundLevel(category, prevVolumes[category.ordinal()]);
+        });
     }
 
 }
