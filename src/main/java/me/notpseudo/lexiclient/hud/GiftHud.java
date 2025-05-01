197c21882cebb4b39465daa6df2875aa5aea03f7
NotPseudo <hongxwen@gmail.com>
2025-05-01 17:27:02 -0400

    Changed example view of the hud to be in line with new features

--- src/main/java/me/notpseudo/lexiclient/hud/GiftHud.java
+++ src/main/java/me/notpseudo/lexiclient/hud/GiftHud.java
@@ -1,7 +1,6 @@
 package me.notpseudo.lexiclient.hud;
 
-import cc.polyfrost.oneconfig.hud.Hud;
-import cc.polyfrost.oneconfig.libs.universal.ChatColor;
+import me.notpseudo.lexiclient.LexiClient;
 import me.notpseudo.lexiclient.config.LexiConfig;
 import me.notpseudo.lexiclient.features.GiftNotifications;
 
@@ -17,11 +16,12 @@ public class GiftHud extends NotificationHud {
     protected void getLines(List<String> lines, boolean example) {
         if (example) {
             lines.add("§6Valueable Item");
-            if (LexiConfig.showSinceClear) lines.add("Good Items Since Last Clear: 4");
+            if (LexiConfig.showItemsSinceClear) lines.add("Good Items Since Last Clear: 3");
+            if (LexiConfig.showSinceClear) lines.add("Good Drops Since Last Clear: 7");
             if (LexiConfig.showSeparateGiftDrops) {
-                lines.add("§6Valueable Item 1: §r1");
-                lines.add("§cValueable Item 2: §r2");
-                lines.add("§5Valueable Item 3: §r1");
+                lines.add("§6Valueable Item 1: 1");
+                lines.add("§cValueable Drop 2: 4");
+                lines.add("§5Valueable Item 3: 2");
             }
             return;
         }
